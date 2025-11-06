da8403ab13cb73f1fe9b51c4fef990bde6d2a2ba
Darkdaisy01 <146349940+Darkdaisy01@users.noreply.github.com>
2025-11-06 17:41:56 +0100

    hola
    g

---
+++ MEMsym.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Función principal
+int main() {
+    // Inicializa variables aquí
+    printf("Hola buenas, este es mi programa en C\n");
+
+    // Tu lógica aquí
+
+    return 0;
+}

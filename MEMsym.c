4b60913b5fa028019453104e6706a359a13f99e2
Darkdaisy01 <146349940+Darkdaisy01@users.noreply.github.com>
2025-11-22 12:03:27 +0100

    Declaración de funciones y resto de variables

--- MEMsym.c
+++ MEMsym.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> 
 
+#define NUMFILAS 8
 #define TAM_LINEA 16
+#define RAM_SIZE 4096
 
 
 typedef struct {
@@ -8,14 +13,20 @@ unsigned char ETQ;
 unsigned char Data[TAM_LINEA];
 } T_CACHE_LINE;
 
+void LimpiarCACHE(T_CACHE_LINE tbl[NUMFILAS]);
+void VolcarCACHE(T_CACHE_LINE tbl[NUMFILAS]);
+void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
+void TratarFallo(T_CACHE_LINE tbl[NUMFILAS], unsigned char *MRAM, int ETQ, int linea, int bloque);
+
 
 int main() {
     
     int globaltime = 0;
     int numfallos = 0;
-    
-    printf("BOOOOMM\n");
+    T_CACHE_LINE cache[NUMFILAS];
+    unsigned char SimulRAM[RAM_SIZE];
 
+    printf("BOOOOMM\n");
 
     return 0;
 }

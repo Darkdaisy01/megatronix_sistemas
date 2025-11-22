94213c56d6a4b770f60cc774f2eaf26408dc18c8
Darkdaisy01 <146349940+Darkdaisy01@users.noreply.github.com>
2025-11-22 12:20:32 +0100

    Lectura  y uso de archivos

--- MEMsym.c
+++ MEMsym.c
@@ -28,5 +28,35 @@ int main() {
 
     printf("BOOOOMM\n");
 
+    //Usar dos bucles para poner a 0xFF (hexadecimal) el campo etiqueta (ETQ) y a 0x23 el campo de datos de cada línea de la caché.
+    for (int i = 0; i < NUMFILAS; i++) {
+        cache[i].ETQ = 0xFF;
+        for (int j = 0; j < TAM_LINEA; j++) {
+            cache[i].Data[j] = 0x23;
+        }
+    }
+
+    //Cargar el archivo binario CONTENTS_RAM.bin en SimulRAM
+    FILE *file_ram = fopen("CONTENTS_RAM.bin", "rb");
+    if (file_ram == NULL) {
+        printf("ERROR: No se puede abrir CONTENTS_RAM.bin\n");
+        return -1;
+    }
+    size_t leidos = fread(SimulRAM, sizeof(unsigned char), RAM_SIZE, file_ram);
+    if(leidos != RAM_SIZE) {
+        printf("ERROR: No se pudo leer la RAM completa\n");
+        fclose(file_ram);
+        return -1;
+    }
+    fclose(file_ram);
+
+
+
+    FILE *file_adress = fopen("addresses.txt", "r");
+    if (file_adress == NULL){
+        printf("ERROR: no se ha podido abrir el archivo addresses.txt\n");
+        return -1;
+    }
+
     return 0;
 }
